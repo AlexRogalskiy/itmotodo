1ef6af8d52b1c5ab3ff8153b365cf3fd18da326e
Vadim Gorbachev <bmsdave@gmail.com>
2022-01-28 00:10:53 +0300

    main

---
+++ app/src/main/java/com/itmo/todoapp/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.itmo.todoapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.itmo.todoapp.ui.add_edit_todo.AddEditTodoScreen
+import com.itmo.todoapp.ui.theme.TodoAppTheme
+import com.itmo.todoapp.ui.todo_list.TodoListScreen
+import com.itmo.todoapp.util.Routes
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TodoAppTheme {
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = Routes.TODO_LIST
+                ) {
+                    composable(Routes.TODO_LIST) {
+                        TodoListScreen(
+                            onNavigate = {
+                                navController.navigate(it.route)
+                            }
+                        )
+                    }
+                    composable(
+                        route = Routes.ADD_EDIT_TODO + "?todoId={todoId}",
+                        arguments = listOf(
+                            navArgument(name = "todoId") {
+                                type = NavType.IntType
+                                defaultValue = -1
+                            }
+                        )
+                    ) {
+                        AddEditTodoScreen(onPopBackStack = {
+                            navController.popBackStack()
+                        })
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/itmo/todoapp/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.itmo.todoapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFFF00FF)
+val Purple500 = Color(0xFFFF0000)
+val Purple700 = Color(0xFF00FF00)
+val Teal200 = Color(0xFF0000FF)
