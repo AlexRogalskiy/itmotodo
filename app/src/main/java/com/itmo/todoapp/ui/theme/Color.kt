7417bce125e6aa879e5da178f846ac9525ebd44d
Vadim Gorbachev <bmsdave@gmail.com>
2022-01-28 00:16:12 +0300

    change colors

--- app/src/main/java/com/itmo/todoapp/ui/theme/Color.kt
+++ app/src/main/java/com/itmo/todoapp/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.itmo.todoapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFFF00FF)
-val Purple500 = Color(0xFFFF0000)
-val Purple700 = Color(0xFF00FF00)
-val Teal200 = Color(0xFF0000FF)
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
